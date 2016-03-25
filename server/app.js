097a21b5f690d3c71f897cb398a7fd5b16373fdb
mdometita <mdometita@bluekai.com>
2016-03-25 14:50:53 -0700

    Fixed calculation issue by converting HTTP params to numbers

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 var http = require('http'),
     url = require('url'),
-    svg2png = require("svg2png"),
     slSvg = require('./sparklineSvg');
 
 var port = process.argv[2] ? process.argv[2] : 9000
@@ -11,28 +10,21 @@ var server = http.createServer( (request, response) => {
     if (! requestInfo.query['values']) {
     	response.end('')
     } else {
-    	var values = requestInfo.query['values'].split(',')
-        var width = requestInfo.query['w'] ? requestInfo.query['w'] : 100
-        var height = requestInfo.query['h'] ? requestInfo.query['h'] : 20
+        var values = requestInfo.query['values'].split(',').map(x => parseFloat(x))
+        var width = parseInt(requestInfo.query['w'] ? requestInfo.query['w'] : 100)
+        var height = parseInt(requestInfo.query['h'] ? requestInfo.query['h'] : 20)
         var format = requestInfo.query['format'] ? requestInfo.query['format'] : 'svg'
-
-    	console.log(requestInfo)
 
         console.log('Params: w=' + width + ', h=' + height + ', format=' + format + ', values=' + values.join())
         slSvg(values, width, height, (err, svg) => {
                 console.log(svg)
                 if (format === 'html') {
                 	response.setHeader('Content-Type', 'text/html')
-                	response.end('<html>' + svg + '</html>')
+                	response.end('<html><body style="margin: 0">' + svg + '</body></html>')
               	} else if (format === 'png') {
-					console.log('png')
-                	response.setHeader('Content-Type', 'image/png')
-              		svg2png(new Buffer(svg, 'utf-8'))
-              			.then( buffer => {
-	   						console.log("Writing png buffer")
-	                        response.end(buffer)
-						})
-						.catch(e => console.error(e))
+                	response.setHeader('Content-Type', 'text/plain')
+                    response.statusCode = 400
+              		response.end('Not yet supported') // future
                 } else {
                 	response.setHeader('Content-Type', 'image/svg+xml')
                 	response.end(svg)
